8ca1c84d07a0581d4a8ff12fee29d2b67e04275f
Lisa Koivu <lkoivu@healthline.com>
2018-11-13 14:23:07 -0500

    Now that I have a better understanding of os.walk...

--- gen-graph.py
+++ gen-graph.py
@@ -29,54 +29,89 @@ for app in os.listdir(start_dir):
     # if os.path.isdir(start_dir + "/" + app):
     #     add_cluster(app)
 
-    for dir in os.listdir(start_dir + "/" + app):
-        g = Digraph(name=dir)
-        for dir2 in os.listdir(start_dir + "/" + app + "/" + dir):
-            if os.path.isdir(start_dir + "/" + app + "/" + dir + "/" + dir2):
-                gg = Digraph(name=dir2)
-                g.subgraph(gg)
+#    for dir in os.listdir(start_dir + "/" + app):
+    for root, dir, files in os.walk(start_dir + "/" + app):
+        for dir in os.listdir(start_dir + "/" + app + "/"):
+            g = Digraph(name=dir)
+
+            # if os.path.isdir(root + "/" + dir ):
+            #     gg = Digraph(name=dir)
+            #     g.subgraph(gg)
+
+            for file in files:
+                if file.endswith(".tfvars"):
+                    with open(root + "/" + file, 'r') as fh:
+                        obj=hcl.load(fh)
+                        try:
+
+                            mod_version = obj['terragrunt']['terraform']['source'].split("ref=")
+                        except Exception as e:
+                            print('No terragrunt/terraform/source in this file {}'.format(e))
+                            continue
+
+                        try:
+                            label = root.split('/')[-1]
+                            G.node(label)
+                            G.node_attr['fillcolor'] = 'lightgrey'
+                            # n = G.get_node(label)
+
+
+                        except Exception as e:
+                            print('Exception {}'.format(e))
+
+
+                        try:
+                            paths=obj['terragrunt']['dependencies']['paths']
+                            referenced=str(paths[-1]).split('../')
+                            source = mod_version[0].split('//')
+                            print("graphing {} : {}".format(source[-1], str(referenced[-1])))
+
+                            G.edge(source[-1], referenced[-1])
+
+                        except Exception as e:
+                            print("edge exception {}".format(e))
         G.subgraph(g)
 
 
-    for root, dir, files in os.walk(start_dir + "/" + app):
-
-        print('stop')
-
-        # g = Digraph (name=dir)
-        # # g.edge(dir, app)
-        # # G.subgraph(g)
-
-        for items in fnmatch.filter(files, "*.tfvars"):
-            # print (root)
-            with open(root + "/" + items, 'r') as file:
-                obj=hcl.load(file)
-                try:
-
-                    mod_version = obj['terragrunt']['terraform']['source'].split("ref=")
-                except Exception as e:
-                    print('No terragrunt/terraform/source in this file {}'.format(e))
-                    continue
-
-                try:
-                    label = root.split('/')[-1]
-                    G.node(label)
-                    G.node_attr['fillcolor'] = 'lightgrey'
-                    # n = G.get_node(label)
-
-
-                except Exception as e:
-                    print('Exception {}'.format(e))
-
-
-                try:
-                    paths=obj['terragrunt']['dependencies']['paths']
-                    referenced=str(paths[-1]).split('../')
-                    source = mod_version[0].split('//')
-                    print("graphing {} : {}".format(source[-1], str(referenced[-1])))
-
-                    G.edge(source[-1], referenced[-1])
-
-                except Exception as e:
-                    print("edge exception {}".format(e))
+    # for root, dir, files in os.walk(start_dir + "/" + app):
+    #
+    #     print('stop')
+    #
+    #     # g = Digraph (name=dir)
+    #     # # g.edge(dir, app)
+    #     # # G.subgraph(g)
+    #
+    #     for items in fnmatch.filter(files, "*.tfvars"):
+    #         # print (root)
+    #         with open(root + "/" + items, 'r') as file:
+    #             obj=hcl.load(file)
+    #             try:
+    #
+    #                 mod_version = obj['terragrunt']['terraform']['source'].split("ref=")
+    #             except Exception as e:
+    #                 print('No terragrunt/terraform/source in this file {}'.format(e))
+    #                 continue
+    #
+    #             try:
+    #                 label = root.split('/')[-1]
+    #                 G.node(label)
+    #                 G.node_attr['fillcolor'] = 'lightgrey'
+    #                 # n = G.get_node(label)
+    #
+    #
+    #             except Exception as e:
+    #                 print('Exception {}'.format(e))
+    #
+    #
+    #             try:
+    #                 paths=obj['terragrunt']['dependencies']['paths']
+    #                 referenced=str(paths[-1]).split('../')
+    #                 source = mod_version[0].split('//')
+    #                 print("graphing {} : {}".format(source[-1], str(referenced[-1])))
+    #
+    #                 G.edge(source[-1], referenced[-1])
+    #
+    #             except Exception as e:
+    #                 print("edge exception {}".format(e))
 
 G.view('test.dot',start_dir)
